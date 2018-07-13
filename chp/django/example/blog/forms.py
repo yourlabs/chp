2c5152718f6f4f1d8fb0774b9ecab66b6638d4ed
jpic <jamespic@gmail.com>
2018-07-14 01:37:43 +0200

    Repackage scripts

--- chp/django/example/blog/forms.py
+++ chp/django/example/blog/forms.py
@@ -1,26 +1,24 @@
+import chp
+
 from django import forms
 
 from .models import Post
-from . import chip
-from . import pyreact
 from django.utils.safestring import mark_safe
 
 def FormSchema(is_checked):
-    return chip.Form([
-                chip.Row([
-                    chip.Input('username'),
-                    chip.CheckboxField(is_checked),
-                ])
-            ])
+    return chp.Form([
+        chp.Row([
+            chp.Input('username'),
+            chp.CheckboxField(is_checked),
+        ])
+    ])
 
 
 class PostForm(forms.ModelForm):
     def render(self):
         is_checked = 'checked' # self.checked
         Form = FormSchema(is_checked)
-
-        html = pyreact.render_element(Form)
-
+        html = chp.render_element(Form)
         return mark_safe(html)
 
     class Meta:
---
+++ chp/django/example/settings.py
@@ -0,0 +1,49 @@
+import os
+
+# FORM_RENDERER = 'chp.django.mdc.renderer.FormRenderer'
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'notsecret')
+DEBUG = os.getenv('DEBUG', False)
+ROOT_URLCONF = 'chp.django.example.urls'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'chp.django.example.blog',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+STATIC_URL = '/static/'
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+
+]
