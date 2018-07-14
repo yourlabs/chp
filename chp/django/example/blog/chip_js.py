e82c4c6344e7b975592512f2af7a9aab9cd3450a
Thomas Binetruy <tbinetruy@gmail.com>
2018-07-14 16:53:02 +0200

    created first proto

--- chp/django/example/blog/chip_js.py
+++ chp/django/example/blog/chip_js.py
@@ -13,6 +13,14 @@ def instruction(value):
     children = value if type(value) is str else [value]
     return cjs(props, children)
 
+def block(value):
+    props = [
+        cp('before', '{'),
+        cp('after', '}'),
+    ]
+    children = value if type(value) is str else [value]
+    return cjs(props, children)
+
 def def_global(name, value):
     props = [
         cp('before', f'window.{name}='),
@@ -44,6 +52,20 @@ def def_func(name, arguments, children):
     ]
     return cjs(props, children)
 
+def If(condition, children):
+    props = [
+        cp('before', 'if(' + condition + ') {'),
+        cp('after', '}'),
+    ]
+    return cjs(props, children)
+
+def Return(children):
+    props = [
+        cp('before', 'return '),
+        cp('after', ''),
+    ]
+    return cjs(props, [instruction(children)])
+
 def op(operation, operand, operee):
     props = [
         cp('before', f'({operand} {operation} '),
@@ -52,12 +74,21 @@ def op(operation, operand, operee):
     children=""
     return cjs(props, children)
 
+
+i = instruction(op('+', '1', '2')),
 content = [
-    def_local('a', '1'),
-    def_global('a', '1'),
-    instruction(op('+', '1', '2')),
+    # def_local('a', '1'),
+    # def_global('a', '1'),
+    # instruction(op('+', '1', '2')),
+    # If('a === 2', i),
+    def_local('x', 'document.getElementById(\'mySelect\').value'),
+    assign('document.getElementById(\'demo\').innerHTML', op('+', "'You selected: '", 'x')),
+    Return('a')
 ]
 ast = def_local('a', '1')
-ast = def_global('foo', def_func("foo", "a, b", [def_func("bar", "c, d, e", content)]))
+# ast = def_global('foo', def_func("foo", "a, b", [def_func("bar", "c, d, e", content)]))
+ast = def_func("f", "", content)
 js = chp.render_js_element(ast)
 print(js)
+
+# (function() {var x = })
