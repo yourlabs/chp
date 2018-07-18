8da8def9d5b1600cf5363e7cdc6cc623bfec0476
Thomas Binetruy <tbinetruy@gmail.com>
2018-07-18 22:02:17 +0200

    renamed

--- chp/django/example/blog/chp_build.py
+++ chp/django/example/blog/chp_build.py
@@ -506,17 +506,13 @@ def patch_dom(patches):
 
 def render_app(store_name, store_content_json):
     return progn([
-        def_local("old_asttt", "window.asttt ? window.asttt : JSON.parse(document.querySelector(\"[chp-id='chp-ast']\").innerHTML)"),
-        def_local("new_asttt", f"inject_ids(FormSchema(window.{store_name}, '{store_content_json}'))"),
-        def_local("[patches, new_ast_from_diff]", "old_asttt ? window.diff_asts(old_asttt, new_asttt) : false"),
-        def_global("asttt", "new_ast_from_diff"),
-        progn("patches ? patch_dom(patches) : false"),
-        def_local("html", f"render_element(window.asttt)"),
-        # assign("document.querySelector('body').innerHTML", "html"),
+        def_local("old_chp_ast", "window.chp_ast ? window.chp_ast : JSON.parse(document.querySelector(\"[chp-id='chp-ast']\").innerHTML)"),
+        def_local("new_chp_ast", f"inject_ids(FormSchema(window.{store_name}, '{store_content_json}'))"),
+        def_local("[patches, new_ast_from_diff]", "old_chp_ast ? window.diff_asts(old_chp_ast, new_chp_ast) : false"),
+        def_global("chp_ast", "new_ast_from_diff"),
+        progn("patch_dom(patches)"),
         progn("eval(document.querySelector('body script').innerHTML);"),
     ])
-
-
 
 
 ####### todos.py
@@ -631,13 +627,13 @@ def AST(json_ast):
 
 
 def injectAstIntoDOM(ast):
-    window.asttt = ast
+    window.chp_ast = ast
 
 
 def Inject_ast_into_DOM(app, json_ast):
     children = [
         app,
         AST(json_ast),
-        Script("window.asttt = JSON.parse(document.querySelector(\"[chp-id='chp-ast']\").innerHTML)"),
+        Script("window.chp_ast = JSON.parse(document.querySelector(\"[chp-id='chp-ast']\").innerHTML)"),
     ]
     return Div([], children)
