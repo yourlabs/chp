3da997d28bc21dea0c51f271db20a0936f9f8abe
Thomas Binetruy <tbinetruy@gmail.com>
2018-07-20 13:49:13 +0200

    create eample

--- chp/django/example/blog/forms.py
+++ chp/django/example/blog/forms.py
@@ -1,39 +1,20 @@
 from django import forms
 
-from . import chp_build
+from . import chp
+from . import Form
+
 from .models import Post
 from django.utils.safestring import mark_safe
 
 
 class PostForm(forms.ModelForm):
     def render(self):
-        is_checked = {
-            "name": "hello",
-        }
         ctx={
-            "label": "Labelling:",
-            "password": "bar"
+            "name": "JPic",
+            "password": "Aaroon et Mia"
         }
-        store = {
-            "name": "this is my first store !!",
-            "todos": [
-                {
-                    "name": "hello",
-                    "id": "0",
-                },
-                {
-                    "name": "todo number secondo !!",
-                    "id": "1",
-                },
-            ]
-        }
-        import json
-        ast = chp_build.FormSchema(store, json.dumps(store))
-        form = chp_build.inject_ids(ast)
-        app = chp_build.Inject_ast_into_DOM(form, json.dumps(form))
-        html = chp_build.render_element(app, chp_build.context_middleware(ctx))
-        print(html)
-        print(form)
+        ast = Form.Form()
+        html = chp.render_element(ast, chp.context_middleware(ctx))
         return mark_safe(html)
 
     class Meta:
