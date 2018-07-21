419947fdfb66a4971eb997076b58a9c65de52c55
jpic <jamespic@gmail.com>
2018-07-21 02:40:58 +0200

    Fix yarn pipeline

--- chp/django/example/webpack.config.js
+++ chp/django/example/webpack.config.js
@@ -1,27 +1,31 @@
 const { spawnSync } = require('child_process');
+var path = require('path')
 var fs = require('fs');
-const child = spawnSync('chp', ['generate', '--entry-point', 'webpack.entry.py']);
-fs.writeFile('webpack.bundle.py', child.output.join('\n'), function(err) {
+const child = spawnSync('chp',
+  ['generate', '--entry-point', 'webpack.entry.py'], {maxBuffer: 20000*1024}
+);
+fs.writeFile('webpack_bundle.py', child.stdout, function(err) {
   if(err) {
       return console.log(err);
   }
 });
 
 module.exports = {
-    mode: 'development',
-    entry: './webpack.bundle.py',
-    output: {
-      filename: 'lol.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.py$/,
-                loader: 'py-loader',
-                options: {
-                    compiler: 'transcrypt'
-                }
-            }
-        ]
-    }
+  mode: 'development',
+  entry: './webpack_bundle.py',
+  output: {
+    filename: 'todos.js',
+    path: path.resolve(__dirname, 'todos/static')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.py$/,
+        loader: 'py-loader',
+        options: {
+          compiler: 'transcrypt'
+        }
+      }
+    ]
+  }
 }
